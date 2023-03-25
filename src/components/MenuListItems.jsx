a7f69cb0554661f4eeff9a71d730d2d3f273ce6b
Nese84 <nbserbest84@gmail.com>
2023-03-25 09:23:47 +0300

    menülist

--- src/components/MenuListItems.jsx
+++ src/components/MenuListItems.jsx
@@ -1,4 +1,3 @@
-import IconButton from "@mui/material/IconButton"
 import List from "@mui/material/List"
 import ListItem from "@mui/material/ListItem"
 import ListItemButton from "@mui/material/ListItemButton"
@@ -11,6 +10,7 @@ import StoreIcon from "@mui/icons-material/Store"
 import StarsIcon from "@mui/icons-material/Stars"
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart"
 import SupervisorAccountIcon from "@mui/icons-material/SupervisorAccount"
+import { useNavigate } from "react-router-dom"
 
 const icons = [
   {
@@ -50,16 +50,32 @@ const icons = [
   },
 ]
 
+const iconStyle = {
+  color: "white",
+  "& .MuiSvgIcon-root": { color: "white" },
+  "&:hover": { color: "red" },
+  "&:hover .MuiSvgIcon-root": { color: "red" },
+}
+
 const MenuListItems = () => {
+  const navigate = useNavigate()
   return (
     <div>
       <List>
-        {icons?.map((text, index) => (
-          <ListItem key={text} disablePadding>
-            <ListItemButton>
-              <ListItemIcon></ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItemButton>
+        {icons?.map((item, index) => (
+          <ListItem key={index} disablePadding>
+            {item.url.includes("http") && (
+              <ListItemButton to={item.url} sx={iconStyle}>
+                <ListItemIcon>{item.icon}</ListItemIcon>
+                <ListItemText primary={item.title} />
+              </ListItemButton>
+            )}
+            {!item.url.includes("http") && (
+              <ListItemButton onClick={() => navigate(item.url)} sx={iconStyle}>
+                <ListItemIcon>{item.icon}</ListItemIcon>
+                <ListItemText primary={item.title} />
+              </ListItemButton>
+            )}
           </ListItem>
         ))}
       </List>
