ba964972d73868617b43b40fff9bc0897deb0717
Nese84 <nbserbest84@gmail.com>
2023-03-27 15:32:12 +0300

    firm

--- src/pages/Firms.jsx
+++ src/pages/Firms.jsx
@@ -1,8 +1,13 @@
-
+import Typography from '@mui/material/Typography'
 
 const Firms = () => {
   return (
-    <div>Firms</div>
+    <div>
+      <Typography variant="h4" color="error">
+        Firm
+      </Typography>
+      <Button variant="contained">New Firm</Button>
+    </div>
   )
 }
 
