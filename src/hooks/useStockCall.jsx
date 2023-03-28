6b281fcf1752750eae1db89482328d5e1bd4b7c4
Nese84 <nbserbest84@gmail.com>
2023-03-28 12:37:50 +0300

    firmcard

--- src/hooks/useStockCall.jsx
+++ src/hooks/useStockCall.jsx
@@ -1,35 +1,42 @@
-import axios from 'axios'
+
 import { useDispatch, useSelector } from "react-redux"
 import { fetchFail, getSuccess, fetchStart } from "../features/stockSlice"
-// import { toastErrorNotify, toastSuccessNotify } from "../helper/ToastNotify"
-// import useAxios from "./useAxios"
-
-
+import { toastErrorNotify, toastSuccessNotify } from "../helper/ToastNotify"
+import useAxios from "./useAxios"
 
 const useStockCall = () => {
+  const dispatch = useDispatch()
 
-    const dispatch = useDispatch()
-    const { token } = useSelector((state) => state.auth)
+  const { axiosWithToken } = useAxios()
 
-    const getStockData= async(url)=>{
-        const BASE_URL = "https://12314.fullstack.clarusway.com/"
-    
-        dispatch(fetchStart())
-        const url ="firms"
-    
-    try {  
-        const {data}= await axios(`${BASE_URL}stock/${url}/`, {
-          headers: {Authorization: `Token ${token}`}
-        })
-        dispatch(getSuccess({data, url }))
-      
+  const getStockData = async (url) => {
+   
+    dispatch(fetchStart())
+    try {
+     
+      const { data } = await axiosWithToken(`stock/${url}/`)
+      console.log(data)
+      dispatch(getSuccess({ data, url }))
     } catch (error) {
+      console.log(error)
       dispatch(fetchFail())
-      
     }
-  return (
- {getStockData}
-  )
-}}
+  }
+
+  const deleteStockData = async (url, id) => {
+    dispatch(fetchStart())
+    try {
+      await axiosWithToken.delete(`stock/${url}/${id}/`)
+      toastSuccessNotify(`${url} successfuly deleted`)
+      getStockData(url)
+    } catch (error) {
+      console.log(error)
+      dispatch(fetchFail())
+      toastErrorNotify(`${url} can not be deleted`)
+    }
+  }
+
+  return { getStockData, deleteStockData }
+}
 
 export default useStockCall
--- src/pages/Firms.jsx
+++ src/pages/Firms.jsx
@@ -6,30 +6,38 @@ import FirmCard from "../components/FirmCard"
 import useStockCall from "../hooks/useStockCall"
 import { flex } from "../styles/globalStyle"
 
-const Firms = () => {
-  
- 
 
-      
-  }
+
+const Firms = () => {
+ 
 
   const { getStockData } = useStockCall()
   const { firms } = useSelector((state) => state.stock)
 
-
-  useEffect(()=>{
+  useEffect(() => {
+    
     getStockData("firms")
-
   }, [])
+
+  console.log(firms)
+
   return (
     <div>
-      <Typography variant="h4" color="error">
+      <Typography variant="h4" color="error" mb={3}>
         Firm
       </Typography>
+
       <Button variant="contained">New Firm</Button>
+
+      <Grid container sx={flex}>
+        {firms?.map((firm) => (
+          <Grid item key={firm.id}>
+            <FirmCard firm={firm} />
+          </Grid>
+        ))}
+      </Grid>
     </div>
   )
-  }
-
+}
 
 export default Firms
